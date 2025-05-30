e0b83b308c5226918514ae39462b9bb6c673ffa7
AbdulHannan Mohammed <abdulhannanm85@gmail.com>
2025-05-29 19:47:08 -0700

    new method for light classification, works well. camera temp mounted in lab. can read photo and classify now. server stuff left

--- colorDetection/main.py
+++ colorDetection/main.py
@@ -3,72 +3,99 @@ import numpy as np
 import os
 
 #lets try a dynamic cropping and fall into static cropping if it does not work too well
-def cropStaticFurnaces(imagePath):
+def cropStaticFurnaces(imagePath, tube):
     readImage = cv2.imread(imagePath)
-    x, y, w, h = 290, 475, 50, 100
+    dimensionArray = [[362, 380, 25, 55], [362, 500, 25, 55],  [362,618, 25, 55], [362,737, 25, 55],
+                      [979, 353, 25, 55], [980, 486, 25, 55], [981, 619, 25, 55], [983, 749, 25, 55],
+                      [1403, 349, 25, 55], [1408, 479, 25, 55], [1407, 608, 25, 55], [1408, 736, 25, 55]] 
+
+
+    x, y, w, h = dimensionArray[tube][0], dimensionArray[tube][1], dimensionArray[tube][2], dimensionArray[tube][3]
 
     cropped = readImage[y: y+h, x: x+w]
+    cv2.imwrite("preprocess/croppedimg.jpg",  cropped)
     return cropped
 
-
+#processes the image into black and white
 def detectLight(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    lower_red1 = np.array([0, 100, 100])
-    upper_red1 = np.array([10, 255, 255])
-
-    lower_red2 = np.array([160, 100, 100])
-    upper_red2 = np.array([180, 255, 255])
+    lower_white = np.array([0, 0, 200])
+    upper_white = np.array([180, 40, 255])
 
 
-    lower_orange = np.array([11, 100, 100])
-    upper_orange = np.array([25, 255, 255])
-
-
-    lower_green = np.array([40, 100, 100])
-    upper_green = np.array([80, 255, 255])
-
-
-    mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
-    mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    mask_orange = cv2.inRange(hsv, lower_orange, upper_orange)
-    mask_green = cv2.inRange(hsv, lower_green, upper_green)
-
-    #detects the color using shortcircuiting
-    mask_combined = cv2.bitwise_or(mask_red1, mask_red2) 
-    mask_combined = cv2.bitwise_or(mask_combined, mask_orange) 
-    mask_combined = cv2.bitwise_or(mask_combined, mask_green)
+    mask_combined = cv2.inRange(hsv, lower_white, upper_white)
 
     cv2.imwrite("preprocess/processedImage.png", mask_combined)
     return "preprocess/processedImage.png"
 
-def classifyColor(image):
+#tells us what color is on, based on position of the light
+def classifyColor(image, tube):
     img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
     if img is None:
         print("file not found")
 
     height, width = img.shape
+    
     section = height // 3
 
     top = img[0: section, :]
     middle = img[section : section * 2, :]
     bottom = img[section * 2: , :]
 
-    if(np.any(top == 255)):
-        print("GREEN ON")
-    if(np.any(middle == 255)):
-        print("ORANGE ON")
-    if(np.any(bottom == 255)):
-        print("RED ON")
 
+    top_array = []
+    top_sum = 0
+    for y in range(top.shape[0]):
+        for x in range(top.shape[1]):
+            if(top[y, x] ==  255):
+                top_array.append(1)
+                top_sum+=1
+            else:
+                top_array.append(0)
+    
+    middle_sum = 0
+    middle_array = []
+    for y in range(middle.shape[0]):
+        for x in range(middle.shape[1]):
+            if(middle[y, x] == 255):
+                middle_array.append(1)
+                middle_sum+=1
+            else:
+                middle_array.append(0)
+    
 
+        
+    bottom_sum = 0
+    bottom_array = []
+    for y in range(bottom.shape[0]):
+        for x in range(bottom.shape[1]):
+            if(bottom[y, x] == 255):
+                bottom_array.append(1)
+                bottom_sum+=1
+            else:
+                bottom_array.append(0)
+        
+
+    # print("top sum: ", top_sum)
+    # print("middle_sum: ",  middle_sum)
+    # print("bottom_sum: ",  bottom_sum)
+
+    if(top_sum > 125):
+        print(f"tube {tube+1}: RED ON")
+    if(middle_sum > 125):
+        print(f"tube {tube+1}: ORANGE ON")
+    if(bottom_sum > 125):
+        print(f"tube {tube+1}: GREEN ON")
+
+    print('--------------------------------------------')
 
 def show_coordinates(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f"X: {x}, Y: {y}")
 
-    img = cv2.imread("images/fullfurnace.jpeg")
+    img = cv2.imread("images/highres0.jpg")
     cv2.namedWindow("image")
     cv2.setMouseCallback("image", show_coordinates)
 
@@ -78,6 +105,25 @@ def show_coordinates(event, x, y, flags, param):
             break
 
     cv2.destroyAllWindows()
+
+def capture1080p():
+    cap = cv2.VideoCapture(0)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+    ret, frame = cap.read()
+
+    if not ret:
+        print("cant see")
+    else:
+        height, width = frame.shape[:2]
+        print(height, width)
+
+
+    cv2.imwrite("images/highres.jpg", frame)
+
+    cap.release()
 
 
 
@@ -90,8 +136,14 @@ def show_coordinates(event, x, y, flags, param):
 #     os.remove("preprocess/processedImage.png")
 
 
+for i in range(0, 12):
+    croppedImg = cropStaticFurnaces("images/highres0.jpg", i)
+    filePath = detectLight(croppedImg)
+    classifyColor(filePath, i)
 
-croppedImg = cropStaticFurnaces("images/fullfurnace.jpeg")
-filePath = detectLight(croppedImg)
-classifyColor(filePath)
+# for i in range(5):
+#     capture1080p(i)
+
+
+
 
